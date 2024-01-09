2bdd4db450e17ecb0b7a5f9798b2390582a72fbf
Alisher Zinullayev <alisherzinullayev@Alishers-MacBook-Air.local>
2024-01-09 10:17:13 +0600

    first commit

---
+++ BudgetBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetBuddy
+//
+//  Created by Alisher Zinullayev on 09.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemOrange
+    }
+
+
+}
+
